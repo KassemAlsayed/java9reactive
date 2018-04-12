986a0036444e21c85663e93b7d183909b161527c
kassem.ali <kassem.ali@innovapost.com>
2018-04-12 14:22:49 -0400

    running the app commands

---
+++ run.cmd
@@ -0,0 +1,1 @@
+java $JAVA_HOME/bin/java -classpath ./target/classes com.kassem.java9.reactive.ReactiveMain
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+$JAVA_HOME/bin/java -classpath ./target/classes com.kassem.java9.reactive.ReactiveMain
